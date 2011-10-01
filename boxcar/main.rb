8f4e6ce69b6e008fedf48e465dcf4367e3e70488
James Gifford <james@jamesrgifford.com>
2011-10-01 17:07:06 -0400

    Added boxcar and twitter-bot rvmrc

--- boxcar/main.rb
+++ boxcar/main.rb
@@ -1,6 +1,5 @@
 require 'rubygems'
 require 'boxcar_api'
-
 provider = BoxcarAPI::Provider.new('c1dQjjKHm0pXIrGQmWEb', 'Oo7gJ18sYVVQOAMzoDahzAmYxo8dfqDJ31cUKdJQ')
 provider.subscribe "james@jamesrgifford.com"
-provider.notify("james@jamesrgifford.com", "It worked!")
+provider.notify("james@jamesrgifford.com", "Hello")
